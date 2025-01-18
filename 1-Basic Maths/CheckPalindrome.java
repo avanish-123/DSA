08531c759cde742a426a962cb01e1c487086c7b2
avanish-123 <avanishpandey022@gmail.com>
2025-01-18 21:12:54 +0530

    armstrong number done

--- 1-Basic Maths/CheckPalindrome.java
+++ 1-Basic Maths/CheckPalindrome.java
@@ -1,13 +1,14 @@
 public class CheckPalindrome {
-    static boolean  check(int num){
+    static boolean check(int num) {
         int temp = num;
         int newNum = 0;
-        while(num>0){
-            newNum = newNum*10 + num%10;
-            num/=10;
+        while (num > 0) {
+            newNum = newNum * 10 + num % 10;
+            num /= 10;
         }
-        return temp==newNum;
+        return temp == newNum;
     }
+
     public static void main(String[] args) {
         int num = 1234321;
         System.out.println(check(num));
