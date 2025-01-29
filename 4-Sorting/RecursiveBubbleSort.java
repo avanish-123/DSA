54abdd5699334e7c2a1ba2d5e288622ef2ca2679
avanish-123 <avanishpandey022@gmail.com>
2025-01-29 23:48:31 +0530

    added var to improve TC

--- 4-Sorting/RecursiveBubbleSort.java
+++ 4-Sorting/RecursiveBubbleSort.java
@@ -9,16 +9,19 @@ public class RecursiveBubbleSort {
     } 
     static void bubbleSort(int arr[], int start, int end){
         if(start>=end) return;
+        boolean isSwaped = false;
         for(int i = start; i<end; i++){
             if(arr[i]>=arr[i+1]){
                 swap(arr, i, i+1);
+                isSwaped = true;
             }
         }
+        if(isSwaped==false) return;
         bubbleSort(arr, start, end-1);
 
     }
     public static void main(String[] args) {
-        int[] arr = {3,2,3,4,2,1,3,4,54,22,3,4232};
+        int[] arr = {1,2};
         bubbleSort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
