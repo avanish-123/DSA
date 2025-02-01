4aea0517c0a3c92471e227b571f7ec5a2318db9a
avanish-123 <avanishpandey022@gmail.com>
2025-02-01 22:16:02 +0530

    remove duplicate also done with for loop

--- 5-Arrays/RemoveDuplicatesFromSortedArray.java
+++ 5-Arrays/RemoveDuplicatesFromSortedArray.java
@@ -3,18 +3,28 @@ import java.util.Arrays;
 
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
-        int[] arr = { 1,2,3,4,5,6,7,8,9,9,9};
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10, 10, 10, 10 };
         int i = 0;
         int j = 1;
-        while(i<arr.length-1 && j<arr.length){
-            if(arr[i]!=arr[j]){
-                arr[i+1] = arr[j];
+        while (i < arr.length - 1 && j < arr.length) {
+            if (arr[i] != arr[j]) {
+                arr[i + 1] = arr[j];
                 i++;
             }
             j++;
         }
-        
         System.out.println(Arrays.toString(arr));
         System.out.println(i);
+        // using for loop
+        int[] arr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10, 10, 10, 10 };
+        int l = 0;
+        for (int k = l + 1; k < arr2.length; k++) {
+            if (arr2[l] != arr2[k]) {
+                arr2[l + 1] = arr2[k];
+                l++;
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(l);
     }
 }
