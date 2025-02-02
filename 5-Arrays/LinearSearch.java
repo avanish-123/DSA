ef3295ca88dcdd984493175378629ee2cd1c1809
avanish-123 <avanishpandey022@gmail.com>
2025-02-03 00:21:48 +0530

    refactored

--- 5-Arrays/LinearSearch.java
+++ 5-Arrays/LinearSearch.java
@@ -1,18 +1,16 @@
 public class LinearSearch {
-    static int linearSearch(int arr[], int target){
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i]==target){
+    static int linearSearch(int arr[], int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
                 return i;
             }
         }
         return -1;
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,10};
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 10 };
         System.out.println(linearSearch(arr, 20));
-
-
-
 
     }
 }
