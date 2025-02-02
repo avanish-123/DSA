94a61fd2bee46f0bd468520a6929ed591e1851df
avanish-123 <avanishpandey022@gmail.com>
2025-02-03 00:10:56 +0530

    optimal code refactored

--- 5-Arrays/MoveZerosToEnd.java
+++ 5-Arrays/MoveZerosToEnd.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MoveZerosToEnd {
-    static void swap(int arr[], int indexOne, int indexTwo){
+    static void swap(int arr[], int indexOne, int indexTwo) {
         int temp = arr[indexOne];
         arr[indexOne] = arr[indexTwo];
         arr[indexTwo] = temp;
-    } 
+    }
+
     public static void main(String[] args) {
         int[] arr = { 12, 1, 2, 3, 40, 5, 4, 3, 0, 0, 4, 0, 1, 2, 3 };
         // bruteForce
@@ -27,25 +28,20 @@ public class MoveZerosToEnd {
         }
         System.out.println(Arrays.toString(arr));
 
-
         // optimal
-        int[] nums = { 12, 1, 2, 3, 40, 5, 4, 3, 0, 0, 4, 0, 1, 2, 3 };
+        int[] nums = { 0, 0, 0, 0, 0, 5, 6, 7 };
         int i = 0;
         int j = 0;
         int size = nums.length;
-        while(i<size && j<size){
-            if(nums[i]!=0 && nums[j]!=0){
+        while ((i < size && j < size) || j < size) {
+            if (nums[i] != 0 && nums[j] != 0) {
                 i++;
-                j++;
             }
-            if(nums[j]==0){
-                j++;
-            }
-            if(nums[j]!=0){
+            if (nums[j] != 0) {
                 swap(nums, i, j);
                 i++;
-                j++;
             }
+            j++;
         }
         System.out.println(Arrays.toString(nums));
 
