219b6f7e60b6b1b147bec717ca7abd4861387776
avanish-123 <avanishpandey022@gmail.com>
2025-02-03 16:10:18 +0530

    right shift array by one place done

--- 5-Arrays/FindMissingNumberInAnArray.java
+++ 5-Arrays/FindMissingNumberInAnArray.java
@@ -1,14 +1,14 @@
 public class FindMissingNumberInAnArray {
     public static void main(String[] args) {
         // n 0 to n
-        int[] arr = {1,2,3,4,0,6,7,8};
+        int[] arr = { 1, 2, 3, 4, 0, 6, 7, 8 };
         int n = arr.length;
         // sum of first n integers
-        int totalsum = n*(n+1)/2;
+        int totalsum = n * (n + 1) / 2;
         int arrSum = 0;
-        for(int i = 0; i<n; i++){
-            arrSum+=arr[i];
+        for (int i = 0; i < n; i++) {
+            arrSum += arr[i];
         }
-        System.out.println(totalsum-arrSum);
+        System.out.println(totalsum - arrSum);
     }
 }
