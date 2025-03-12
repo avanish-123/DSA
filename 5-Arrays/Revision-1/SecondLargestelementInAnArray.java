8204e2cf711b2ebc89fe09aaef74f59ef82e5835
avanish-123 <avanishpandey022@gmail.com>
2025-03-12 17:46:57 +0530

    second Largest, smallest and largest element in an array done

---
+++ 5-Arrays/Revision-1/SecondLargestelementInAnArray.java
@@ -0,0 +1,17 @@
+public class SecondLargestelementInAnArray {
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,5,6,7,6,5,8,99,4};
+        int max = -1;
+        int secMax = -2;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]>max){
+                secMax = max;
+                max = arr[i];
+            }
+            if(arr[i]<max && arr[i]>secMax){
+                secMax = arr[i];
+            }
+        }
+        System.out.println(secMax);
+    }
+}
